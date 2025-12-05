51fa0003d0db6c8155189e10542c32985e51c7f0
Maddie Maslyar <madeline.maslyar@gmail.com>
2025-12-05 14:36:10 -0800

    Add classes

---
+++ class04/class04.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "Class 4 Lab"
+#' author: "Maddie Maslyar (PID A69042845)"
+#' ---  
+
+# R script
+x <- 1:50
+plot(x)
+plot(x, col="violet")
+plot(x, sin(x), typ="l", col = "turquoise", lwd=3)
+
+log(10, 10)
